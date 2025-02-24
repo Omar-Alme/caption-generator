194a1396c8c2bc39fa0608c48b0fa527206174bc
Omar-Alme <omar.almehdar17@gmail.com>
2025-02-24 02:25:36 +0100

    style floating navbar

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -1,15 +1,15 @@
 export default function Navbar() {
     return (
-        <nav className="space-x-6 hidden md:block">
-                <a href="#" className="hover:text-gray-400">
-                    Resources
-                </a>
-                <a href="#" className="hover:text-gray-400">
-                    Pricing
-                </a>
-                <a href="#" className="hover:text-gray-400">
-                    Blog
-                </a>
-            </nav>
-    )
+        <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-gray-900/70 backdrop-blur-sm rounded-full px-8 py-3 space-x-6 hidden md:flex">
+            <a href="#" className="text-white hover:text-gray-400">
+                Resources
+            </a>
+            <a href="#" className="text-white hover:text-gray-400">
+                Pricing
+            </a>
+            <a href="#" className="text-white hover:text-gray-400">
+                Blog
+            </a>
+        </nav>
+    );
 }
